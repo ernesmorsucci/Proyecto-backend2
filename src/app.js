2fe74450fd2113ea8bced7508bf04704a09a9f4f
ErnesDev <ernesmorsucci14@gmail.com>
2026-01-15 10:11:02 -0300

    fixed errors, added passport, cookies and env configs

--- src/app.js
+++ src/app.js
@@ -1,17 +1,33 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
-import dotenv from 'dotenv';
+import { env } from './config/environment.js'
 import { connectDB } from './database/db.js';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import { initializePassport } from './config/passport.js';
+import { serverRoot } from './utils.js';
+import userRouter from './routes/users.router.js';
+import sessionsRouter from './routes/sessions.router.js';
 
-dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = env.PORT;
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './src/views');
+app.set('views', serverRoot + '/views');
+
+app.use(express.json());
+app.use(express.static(serverRoot + '/public'))
+app.use(cookieParser(env.COOKIE_SECRET));
+
+initializePassport();
+app.use(passport.initialize());
+
+//routes
+app.use('/api/users', userRouter);
+app.use('/api/sessions', sessionsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
-})
+});
