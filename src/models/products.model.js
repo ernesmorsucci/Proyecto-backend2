aeb83183bcd39f0b91ac07c608e6fda2185a1e40
ErnesDev <ernesmorsucci14@gmail.com>
2026-02-11 18:32:25 -0300

    Product model added

---
+++ src/models/products.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+// import { paginate } from "mongoose-paginate-v2";
+
+const productsSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  code: { type: String, required: true, unique: true },
+  price: { type: Number, required: true },
+  status: { type: Boolean, default: true },
+  stock: { type: Number, required: true },
+  category: { type: String, required: true },
+  thumbnails: { type: [String], default: [] }
+});
+
+// productsSchema.plugin(paginate);
+export const productsModel = mongoose.model("products", productsSchema);
