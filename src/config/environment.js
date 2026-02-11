f6d923902d2bd275774150f9054295a97b8e9887
ErnesDev <ernesmorsucci14@gmail.com>
2026-02-11 18:37:19 -0300

    New mailing credentials

--- src/config/environment.js
+++ src/config/environment.js
@@ -6,5 +6,7 @@ export const env = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
-  COOKIE_SECRET: process.env.COOKIE_SECRET
+  COOKIE_SECRET: process.env.COOKIE_SECRET,
+  MAILING_ACCOUNT: process.env.MAILING_ACCOUNT,
+  MAILING_PASS: process.env.MAILING_PASS
 }
